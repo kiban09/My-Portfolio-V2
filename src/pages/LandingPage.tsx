7605e6fddb5330a64d60f9a317e8e902eb6bfa4e
kiban09 <kevintom111294@gmail.com>
2025-07-03 03:07:50 +0800

    added animation on landing page

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -2,12 +2,25 @@ import { Box, Typography, useTheme, Stack, IconButton, Button, Tooltip } from "@
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import FacebookIcon from "@mui/icons-material/Facebook";
-import LocalFireDepartmentIcon from '@mui/icons-material/LocalFireDepartment';
-import ViewInArOutlinedIcon from '@mui/icons-material/ViewInArOutlined';
-import DownloadIcon from '@mui/icons-material/FileDownloadOutlined';
+import LocalFireDepartmentIcon from "@mui/icons-material/LocalFireDepartment";
+import ViewInArOutlinedIcon from "@mui/icons-material/ViewInArOutlined";
+import DownloadIcon from "@mui/icons-material/FileDownloadOutlined";
+import profile from "../assests/hero.png"
+import { useState, useEffect } from "react";
 
 export const LandingPage = () => {
   const theme = useTheme();
+  const [showCircle, setShowCircle] = useState(false);
+  const [showButtons, setShowButtons] = useState(false);
+
+  useEffect(() => {
+    const timer1 = setTimeout(() => setShowCircle(true), 800);
+    const timer2 = setTimeout(() => setShowButtons(true), 500);
+    return () => {
+      clearTimeout(timer1);
+      clearTimeout(timer2);
+    };
+  }, []);
 
   return (
     <Box
@@ -15,85 +28,203 @@ export const LandingPage = () => {
         minHeight: "100vh",
         bgcolor: theme.palette.background.gradient,
         display: "flex",
-        flexDirection: "column",
         alignItems: "center",
         justifyContent: "center",
-        textAlign: "center",
-        p: 2,
+        p: 3,
       }}
     >
-      <Typography variant="h3" color="theme.palette.text.secondary" sx={{fontWeight: 700,}} gutterBottom>
-        Hi, I'm Kevin
-      </Typography>
-
-      <Typography
-        variant="h3"
-        sx={{
-          color: theme.palette.text.primary,
-          fontWeight: 700,
-          mb: 2,
-          fontSize: { xs: "2rem", sm: "2.5rem", md: "3rem" },
-        }}
+      <Stack
+        direction={{ xs: "column-reverse", md: "row" }}
+        spacing={6}
+        alignItems="center"
+        justifyContent="center"
+        sx={{ maxWidth: "1200px", width: "100%", textAlign: { xs: "center", md: "left" } }}
       >
-        an aspiring{" "}
+        {/* Text and Buttons */}
+        <Box sx={{ flex: 1, maxWidth: { xs: "100%", md: "500px" } }}>
+          <Typography
+            variant="h3"
+            sx={{
+              fontWeight: 700,
+              color: theme.palette.text.secondary,
+              mb: 1,
+              wordBreak: "break-word",
+            }}
+          >
+            Hi, I'm Kevin
+          </Typography>
+
+          <Typography
+            variant="h3"
+            sx={{
+              color: theme.palette.text.secondary,
+              fontWeight: 700,
+              mb: 2,
+              fontSize: { xs: "2rem", sm: "2.5rem", md: "3rem" },
+              display: "flex",
+              flexWrap: "wrap",
+              justifyContent: { xs: "center", md: "flex-start" },
+            }}
+          >
+            an aspiring&nbsp;
+            <Box
+              component="span"
+              sx={{
+                color: theme.palette.primary.main,
+                whiteSpace: "nowrap",
+              }}
+            >
+              &lt;Web Developer /&gt;
+            </Box>
+          </Typography>
+
+          <Button
+            variant="outlined"
+            color="primary"
+            rel="noopener noreferrer"
+            href="/My-Portfolio-V2/CV_Tom.pdf"
+            target="_blank"
+            sx={{
+              mt: 2,
+              gap: 1,
+              opacity: showButtons ? 1 : 0,
+              transform: showButtons ? "translateY(0)" : "translateY(20px)",
+              transition: "opacity 1s ease, transform 1s ease",
+            }}
+          >
+            View / Download CV
+            <DownloadIcon />
+          </Button>
+
+          <Stack
+            direction="row"
+            spacing={2}
+            sx={{ mt: 4, flexWrap: "wrap" }}
+            justifyContent={{ xs: "center", md: "flex-start" }}
+          >
+            {[
+              { title: "Facebook", icon: <FacebookIcon />, link: "https://www.facebook.com/kevin.tom.16" },
+              { title: "LinkedIn", icon: <LinkedInIcon />, link: "https://linkedin.com/in/kevin-tom-42ba60261" },
+              { title: "Github", icon: <GitHubIcon />, link: "https://github.com/kiban09" },
+              { title: "FreeCodeCamp", icon: <LocalFireDepartmentIcon />, link: "https://www.freecodecamp.org/Kiban09" },
+              { title: "CodePen", icon: <ViewInArOutlinedIcon />, link: "https://codepen.io/Kevin-Tom-the-builder" },
+            ].map((item, index) => (
+              <Tooltip title={item.title} key={item.title}>
+                <IconButton
+                  href={item.link}
+                  target="_blank"
+                  color="primary"
+                  sx={{
+                    border: `1px solid ${theme.palette.primary.main}`,
+                    opacity: showButtons ? 1 : 0,
+                    transform: showButtons ? "translateY(0)" : "translateY(20px)",
+                    transition: "opacity 1s ease, transform 1s ease",
+                    transitionDelay: `${0.3 + index * 0.2}s`,
+                  }}
+                >
+                  {item.icon}
+                </IconButton>
+              </Tooltip>
+            ))}
+          </Stack>
+        </Box>
+
+        {/*Circular Image */}
         <Box
-          component="span"
           sx={{
-            color: theme.palette.primary.main,
-            display: { xs: "block", sm: "block" },
+            position: "relative",
+            width: { xs: 200, sm: 250, md: 320, lg: 400 },
+            height: { xs: 200, sm: 250, md: 320, lg: 400 },
+            borderRadius: "50%",
+            overflow: "hidden",
+            flexShrink: 0,
+            mx: { xs: "auto", md: 0 },
+            mb: { xs: 4, md: 0 },
+            opacity: showCircle ? 1 : 0,
+            transition: "opacity 5s ease, transform 5s ease",
+            transitionDelay: showCircle ? "1s" : "0s",
           }}
         >
-          &lt;Web Developer /&gt;
+          <Box
+            component="img"
+            src= {profile}
+            alt="Kevin"
+            sx={{
+              width: "100%",
+              height: "100%",
+              objectFit: "cover",
+              borderRadius: "50%",
+              opacity: showCircle ? 1 : 0,
+              transform:"scale(0.96)", 
+              transition: "opacity 5s ease, transform 5s ease",
+              transitionDelay: "2s"
+            }}
+          />
+          {/* Dark Gradient Overlay */}
+          <Box
+            sx={{
+              position: "absolute",
+              bottom: 0,
+              left: 0,
+              width: "100%",
+              height: "60%", 
+              background: "linear-gradient(to top, rgba(0,0,0,1), rgba(0,0,0,0))",
+              opacity: showCircle ? 1 : 0,
+              transition: "opacity 5s ease, transform 5s ease",
+              transitionDelay: "2s",
+              pointerEvents: "none",
+              borderRadius: "50%",
+              zIndex: 2,
+            }}
+          />
+          <Box
+            component="svg"
+            width="100%"
+            height="100%"
+            viewBox="0 0 100 100"
+            sx={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              zIndex: 3,
+              pointerEvents: "none",
+              animation: showCircle ? "rotate 30s linear infinite" : "none",
+              transformOrigin: "center",
+            }}
+          >
+            <circle
+              cx="50"
+              cy="50"
+              r="49"
+              fill="none"
+              strokeLinecap="round"
+              stroke={theme.palette.primary.main}
+              strokeWidth="2"
+            >  
+              <animate
+                attributeName="stroke-dasharray"
+                values="
+                        14 25 2 10;
+                        25 15 6 5;
+                        30 8 10 3;
+                        25 5 6 15;
+                        14 25 2 10"
+                dur="10s"
+                repeatCount="indefinite"
+              />
+            </circle>
+          </Box>
+
+          <style>
+            {`
+              @keyframes rotate {
+                from { transform: rotate(0deg); }
+                to { transform: rotate(360deg); }
+              }
+            `}
+          </style>
         </Box>
-      </Typography>
-
-      
-
-      <Stack direction="row" spacing={2} sx={{ mt: 4 }}>
-        <Button
-          variant="outlined"
-          color="primary"
-          rel="noopener noreferrer"
-          href="/My-Portfolio-V2/CV_Tom.pdf"
-          target="_blank"
-          sx={{ mt: 4, gap: 1 }}
-        >
-          View / Download CV
-          <DownloadIcon />
-        </Button>
-
-        <Tooltip title="Facebook">
-                <IconButton href="https://www.facebook.com/kevin.tom.16" target="_blank" color="primary" sx={{border: `1px solid ${theme.palette.primary.main}`}}>
-                  <FacebookIcon />
-                </IconButton>
-              </Tooltip>
-
-              <Tooltip title="LinkedIn">
-                <IconButton href="https://linkedin.com/in/kevin-tom-42ba60261" target="_blank" color="primary" sx={{border: `1px solid ${theme.palette.primary.main}`}}>
-                  <LinkedInIcon />
-                </IconButton>
-              </Tooltip>
-
-              <Tooltip title="Github">
-                <IconButton href="https://github.com/kiban09" target="_blank" color="primary" sx={{border: `1px solid ${theme.palette.primary.main}`}}>
-                  <GitHubIcon />
-                </IconButton>
-              </Tooltip>
-
-              <Tooltip title="FreeCodeCamp">
-                <IconButton href="https://www.freecodecamp.org/Kiban09" target="_blank" color="primary" sx={{border: `1px solid ${theme.palette.primary.main}`}}>
-                  <LocalFireDepartmentIcon />
-                </IconButton>
-              </Tooltip>
-
-              <Tooltip title="CodePen">
-                <IconButton href="https://codepen.io/Kevin-Tom-the-builder" target="_blank" color="primary" sx={{border: `1px solid ${theme.palette.primary.main}`}}>
-                  <ViewInArOutlinedIcon />
-                </IconButton>
-              </Tooltip>
       </Stack>
-
-      
     </Box>
   );
 };
